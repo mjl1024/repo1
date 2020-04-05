ce403de814e463e9d069b9cc33790bf5a8367769
admin <2693142722@qq.com>
2020-04-05 20:01:14 +0800

    提交到Github

---
+++ Demo/src/com/mjl/Test.java
@@ -0,0 +1,12 @@
+package com.mjl;
+
+/**
+ * @author mjl
+ * create 2020/4/5 19:52
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
